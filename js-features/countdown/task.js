a780a051b03bda8556856df6e2e8d9eb11a134bb
Serg Shkodin <mycrr25@gmail.com>
2023-07-03 11:17:51 +0300

    DZ-2

---
+++ js-features/countdown/task.js
@@ -0,0 +1,8 @@
+const taim = function(){
+if (timer >=0) { alert ("Вы победили в конкурсе!», используя функцию");}
+const timer = document.getElementById("timer");
+timer.textContent -=1;
+};
+
+
+setInterval(taim, 10);
